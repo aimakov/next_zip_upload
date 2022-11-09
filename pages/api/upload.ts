84e5966f9b8f88733a2c398d43af8c5c252d172b
aimakov <nurbolat.aimakov@gmail.com>
2022-11-09 19:30:39 +0900

    Successfully uploaded from backend

---
+++ pages/api/upload.ts
@@ -0,0 +1,85 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import fs from "fs";
+import AdmZip from "adm-zip";
+
+import { NetlifyAPI } from "netlify";
+
+export default async function uploadZip(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const opts = {
+    userAgent: "netlify/js-client",
+    scheme: "https",
+    host: "api.netlify.com",
+    pathPrefix: "/api/v1",
+    accessToken: "pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
+    agent: undefined, // e.g. HttpsProxyAgent
+    globalParams: {}, // parameters you want available for every request.
+    // Global params are only sent of the OpenAPI spec specifies the provided params.
+  };
+
+  const handleSubmission = async (file) => {
+    const response = await axios.post(
+      "https://api.netlify.com/api/v1/sites",
+      file,
+      {
+        headers: {
+          Authorization: "Bearer pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
+          "Content-Type": "application/zip",
+        },
+      }
+    );
+    console.log("Response" + response);
+    return response;
+  };
+
+  // let files = req.body;
+  // fs.writeFile(files.fileName, files.data, "base64", async function (err) {
+  //   if (err) res.status(200).json("Error");
+  //   else {
+  //     const response = await handleSubmission();
+  //     res.status(200).json(response);
+  //   }
+  // });
+
+  // console.log(fs.readFileSync("files.fileName", "utf8"));
+
+  const client = new NetlifyAPI(
+    "pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
+    opts
+  );
+
+  // const zip = new AdmZip("2048.zip");
+
+  const file = fs.createReadStream(`2048.zip`);
+  axios
+    .post("https://api.netlify.com/api/v1/sites", file, {
+      headers: {
+        "Content-Type": "application/zip",
+        Authorization: "Bearer pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
+      },
+    })
+    .then((result) => {
+      console.log(result);
+    });
+
+  // const site = await client.createSite("regal-selkie-a46dba", {
+  //   files: zip,
+  // });
+  // const file = fs.readFile("2048.zip", (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data);
+  // });
+  // const site = await client.createSiteDeploy("serene-arithmetic-41a206", file);
+  // console.log(site);
+
+  // console.log(response);
+
+  // console.log(site);
+
+  // const response = handleSubmission(site);
+
+  res.status(200).json("Done");
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,52 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-    const [file, setFile] = useState();
+  const [file, setFile] = useState();
+  const [uploadFile, setUploadFile] = useState("");
 
-    const changeHandler = (e: any) => {
-        e.preventDefault();
-        setFile(e.target.files[0]);
-    };
+  const changeHandler = (e: any) => {
+    e.preventDefault();
+    setFile(e.target.files[0]);
+  };
 
-    const handleSubmission = async () => {
-        const response = await axios.post("https://api.netlify.com/api/v1/sites", file, {
-            headers: {
-                Authorization: "Bearer pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
-                "Content-Type": "application/zip",
-            },
-        });
+  const handleSubmission = async () => {
+    const response = await axios.post("https://app.netlify.com/sites/", file, {
+      headers: {
+        Authorization: "Bearer pa83q_ldh0eDTF1odouwRuiI5JCIu1swRfZc5G3ogkI",
+        "Content-Type": "application/zip",
+      },
+    });
 
-        console.log(response);
-    };
+    console.log(response);
+  };
 
-    return (
-        <div>
-            <input type="file" name="file" onChange={changeHandler} />
-            <div>
-                <button onClick={handleSubmission}>Submit</button>
-            </div>
-        </div>
-    );
+  // const handleFileReader = (event: React.FormEvent) => {
+  //   let reader = new FileReader();
+  //   reader.readAsDataURL(event.target.files[0]);
+  //   reader.onload = (e) => {
+  //     setUploadFile({
+  //       data: reader.result.split(",").pop(),
+  //       fileName: event.target.files[0].name,
+  //     });
+  //   };
+  // };
+
+  // const reqZip = async () => {
+  //   const response = await axios.post("/api/upload", uploadFile);
+  //   console.log(response.data.default_domain);
+  // };
+
+  const reqZip = async () => {
+    const response = await axios.post("/api/upload", file);
+    console.log(response.data.default_domain);
+  };
+
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      <div>
+        <button onClick={reqZip}>Submit</button>
+      </div>
+    </div>
+  );
 }
